1ba59fb79469e178a8aaf33d27c5e2a45221e227
Juan-AC <acparceria818@gmail.com>
2025-12-19 15:11:09 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,9 @@
+// firebase.js - COMPLETO
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, doc, getDoc, setDoc, updateDoc, deleteDoc, collection, addDoc, getDocs, query, where, orderBy, onSnapshot, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { 
+  getFirestore, doc, getDoc, setDoc, updateDoc, deleteDoc, collection, addDoc, 
+  getDocs, query, where, orderBy, onSnapshot, serverTimestamp 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
@@ -16,4 +20,33 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-export { db, auth, doc, getDoc, setDoc, updateDoc, deleteDoc, collection, addDoc, getDocs, query, where, orderBy, onSnapshot, serverTimestamp };
+// Helpers
+async function getColaborador(matricula) {
+  const docRef = doc(db, 'colaboradores', matricula);
+  return await getDoc(docRef);
+}
+
+function monitorarRotas(callback) {
+  return onSnapshot(collection(db, 'rotas_em_andamento'), snapshot => {
+    const rotas = [];
+    snapshot.forEach(docSnap => {
+      const data = docSnap.data();
+      if (data.ativo !== false) rotas.push({ id: docSnap.id, ...data });
+    });
+    callback(rotas);
+  });
+}
+
+function monitorarAvisos(callback) {
+  const q = query(collection(db, 'avisos'), where("ativo", "==", true));
+  return onSnapshot(q, snapshot => {
+    const dados = snapshot.docs.map(d => ({ id: d.id, ...d.data() }));
+    callback(dados);
+  });
+}
+
+export {
+  db, auth, doc, getDoc, setDoc, updateDoc, deleteDoc, collection, addDoc, getDocs, 
+  query, where, orderBy, onSnapshot, serverTimestamp, 
+  getColaborador, monitorarRotas, monitorarAvisos
+};
