180f5708eb4079161aceedd19dbc5d7a6ea39e31
Juan-AC <acparceria818@gmail.com>
2025-12-19 14:54:53 -0300

    Update maps.js

--- maps.js
+++ maps.js
@@ -1,20 +1,14 @@
 // maps.js
-import { UI } from './ui.js';
-
 let map = null;
 let markers = {};
-let routePolylines = {}; // Para armazenar as linhas de trajeto de cada motorista
+let polylines = {}; // Guarda as linhas de trajeto
 let wakeLock = null;
 
 export const Maps = {
   init: () => {
     if (map) return;
     map = L.map('map').setView([-14.235, -51.925], 4);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap'
-    }).addTo(map);
-    
-    // Expõe a instância do mapa globalmente para ser acessada por onclicks no HTML
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '© OpenStreetMap' }).addTo(map);
     window.mapInstance = map;
   },
 
@@ -30,26 +24,24 @@ export const Maps = {
   atualizarMarcadorMotorista: (id, dados) => {
     const { latitude, longitude, motorista, onibus, rota } = dados;
     
-    // Atualiza ou cria marcador
+    // 1. Marcador (Ônibus)
     if (markers[id]) {
       markers[id].setLatLng([latitude, longitude]);
       markers[id].setPopupContent(`<b>${rota}</b><br>${motorista}<br>${onibus}`);
     } else {
-      const busIcon = L.divIcon({
+      const icon = L.divIcon({
         html: '<i class="fas fa-bus" style="font-size:24px;color:#b00000;text-shadow:1px 1px 2px white"></i>',
-        className: 'custom-bus-icon',
-        iconSize: [30, 30], iconAnchor: [15, 15]
+        className: 'custom-icon', iconSize: [30, 30]
       });
-      markers[id] = L.marker([latitude, longitude], { icon: busIcon })
-        .addTo(map)
+      markers[id] = L.marker([latitude, longitude], { icon }).addTo(map)
         .bindPopup(`<b>${rota}</b><br>${motorista}<br>${onibus}`);
     }
 
-    // Desenha o rastro (Polyline)
-    if (!routePolylines[id]) {
-      routePolylines[id] = L.polyline([], { color: 'blue', weight: 4 }).addTo(map);
+    // 2. Rastro (Polyline) - HISTÓRICO VISUAL
+    if (!polylines[id]) {
+      polylines[id] = L.polyline([], { color: 'blue', weight: 4 }).addTo(map);
     }
-    routePolylines[id].addLatLng([latitude, longitude]);
+    polylines[id].addLatLng([latitude, longitude]);
   },
 
   focarNoOnibus: (lat, lng) => {
