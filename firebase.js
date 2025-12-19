7ae70bbc5d50e2ccf180d636e8bd6a6a61997444
Juan-AC <acparceria818@gmail.com>
2025-12-19 14:55:05 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,6 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { 
-  getFirestore, doc, getDoc, setDoc, updateDoc, deleteDoc, collection, addDoc, 
-  getDocs, query, where, orderBy, onSnapshot, serverTimestamp 
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, doc, getDoc, setDoc, updateDoc, deleteDoc, collection, addDoc, getDocs, query, where, orderBy, onSnapshot, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA5KEaKntt9wPYcy60DutrqvIH34piXsXk",
@@ -20,27 +16,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-// Helpers
-async function getColaborador(matricula) {
-  const docRef = doc(db, 'colaboradores', matricula);
-  return await getDoc(docRef);
-}
-
-// Wrapper para monitoramento
-function monitorarRotas(callback) {
-  return onSnapshot(collection(db, 'rotas_em_andamento'), snapshot => {
-    const rotas = [];
-    snapshot.forEach(docSnap => {
-      const data = docSnap.data();
-      if (data.ativo !== false) rotas.push({ id: docSnap.id, ...data });
-    });
-    callback(rotas);
-  });
-}
-
-// Exportações
-export {
-  db, auth, doc, getDoc, setDoc, updateDoc, deleteDoc, collection, addDoc, getDocs, 
-  query, where, orderBy, onSnapshot, serverTimestamp, 
-  getColaborador, monitorarRotas
-};
+export { db, auth, doc, getDoc, setDoc, updateDoc, deleteDoc, collection, addDoc, getDocs, query, where, orderBy, onSnapshot, serverTimestamp };
